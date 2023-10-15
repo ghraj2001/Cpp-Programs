fa2fa3b2a0670336072a95cbb38341984479e4a1
Raj <66724688+ghraj2001@users.noreply.github.com>
2023-10-15 13:03:42 +0530

    Time: 26 ms (94.49%), Space: 28.2 MB (8.57%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -3,6 +3,6 @@ public:
     int maximumProduct(vector<int>& a) {
          sort(a.begin(),a.end());
         int n=a.size();
-        return max(max(a[0]*a[1]*a[2],a[0]*a[1]*a[n-1]),a[n-1]*a[n-2]*a[n-3]);
+        return max(a[0]*a[1]*a[n-1],a[n-1]*a[n-2]*a[n-3]);
     }
 };
